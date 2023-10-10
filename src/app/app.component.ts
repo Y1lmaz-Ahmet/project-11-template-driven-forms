d20a7b7441df1e6e907c754be1079b24d652a8e4
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-10-10 13:25:32 +0200

    template driven form in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project-11-template-driven-forms';
+
+
+  @ViewChild('myForm',{static:true}) myForm:any
+  firstname:string=""
+  lastname:string=""
+  email: string = ""
+  
+  persons:any[]=[{firstname:this.firstname,lastname:this.lastname,email:this.email}]
+  onSubmit(formValues:any)
+  {
+    console.log(formValues);
+    console.table(formValues.value);
+    this.firstname = formValues.value.firstname
+    this.lastname = formValues.value.lastname
+    this.email = formValues.value.email
+
+    //push the person object in the array
+    this.persons.push({firstname:this.firstname,lastname:this.lastname,email:this.email})
+    //resets the form by clearing the inputs and disabeling the submit button
+    this.myForm.reset()
+  }
 }
